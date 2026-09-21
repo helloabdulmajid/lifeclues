8dc1034c084f5d2b8ed214acd639f2df8fff3ea3
Abdul Majid <helloabdulmajid@gmail.com>
2026-09-21 12:11:28 +0530

    chore: prepare LifeClues backend for deployment

--- src/main/resources/db/migration/V12__add_time_format_to_users.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE users ADD COLUMN time_format VARCHAR(3) DEFAULT 'H12';
-
-UPDATE users SET time_format = 'H12' WHERE time_format IS NULL;
-
-ALTER TABLE users ALTER COLUMN time_format SET DEFAULT 'H12';
-ALTER TABLE users ALTER COLUMN time_format SET NOT NULL;
